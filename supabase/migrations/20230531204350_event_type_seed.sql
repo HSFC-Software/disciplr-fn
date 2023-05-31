1e35c0e176cfbf4a9e3efe0c32ef8fe8a300e717
Sam <svtagalog@joyride.com.ph>
2023-05-31 12:22:31 +0800

    feat: event_type seed

---
+++ supabase/migrations/20230531204350_event_type_seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.event_type (name, description, reference)
+VALUES ('CELLGROUP', 'Cellgroup', 'network_id'),
+       ('CLOSED_CELL', 'Closed Cell', 'network_id'),
+       ('PID', 'PID', 'network_id'),
+       ('CONSOLIDATION', 'Consolidation', 'consolidation_id');
