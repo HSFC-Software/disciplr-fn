0bdb1ca638eee86cd583d37bff904dbe9fb796c6
Sam <svtagalog@joyride.com.ph>
2023-06-10 17:38:11 +0800

    feat(google): get place id

--- supabase/functions/v2/_route/google.ts
+++ supabase/functions/v2/_route/google.ts
@@ -4,14 +4,24 @@ import axiod from "https://deno.land/x/axiod/mod.ts";
 export default () => {
   const router = express.Router();
 
-  router.get("/v2/google/autocomplete", async (req, res) => {
-    const key = Deno.env.get("GOOGLE_PLACE_KEY");
-
+  router.get("/v2/google/place/search", async (req, res) => {
     const { query } = req;
     const response = await axiod.get(
       `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${
         query.input ?? ""
-      }&key=${key}`
+      }&key=${Deno.env.get("GOOGLE_PLACE_KEY")}`
+    );
+
+    res.send(response.data);
+  });
+
+  router.get("/v2/google/place/:id", async (req, res) => {
+    const placeId = req.params.id;
+
+    const response = await axiod.get(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${Deno.env.get(
+        "GOOGLE_PLACE_KEY"
+      )}`
     );
 
     res.send(response.data);
