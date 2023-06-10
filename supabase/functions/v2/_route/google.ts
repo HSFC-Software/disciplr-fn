f622e6f0d4d8d4ebf4f1696341bd349b056c21df
Sam <svtagalog@joyride.com.ph>
2023-06-10 18:13:49 +0800

    feat(google): geocode

--- supabase/functions/v2/_route/google.ts
+++ supabase/functions/v2/_route/google.ts
@@ -1,6 +1,8 @@
 import express from "../../_shared/express.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 
+const key = Deno.env.get("GOOGLE_PLACE_KEY");
+
 export default () => {
   const router = express.Router();
 
@@ -9,19 +11,24 @@ export default () => {
     const response = await axiod.get(
       `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${
         query.input ?? ""
-      }&key=${Deno.env.get("GOOGLE_PLACE_KEY")}`
+      }&key=${key}`
     );
 
     res.send(response.data);
+  });
+
+  router.get("/v2/google/place/geocode", async (req, res) => {
+    const { query } = req;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${query.lat},${query.lng}&key=${key}`;
+    const response = await axiod.get(url);
+    res.send(response.data?.results ?? []);
   });
 
   router.get("/v2/google/place/:id", async (req, res) => {
     const placeId = req.params.id;
 
     const response = await axiod.get(
-      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${Deno.env.get(
-        "GOOGLE_PLACE_KEY"
-      )}`
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${key}`
     );
 
     res.send(response.data);
