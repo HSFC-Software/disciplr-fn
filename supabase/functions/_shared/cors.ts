3e77c8325b205e2014f1d12fb683bb99c6435a2e
Sam <svtagalog@joyride.com.ph>
2023-06-12 02:30:43 +0800

    fix: cors middleware

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -10,16 +10,15 @@ export const cors = (headers: Object = {}) => ({
 
 const corsMiddleware =
   (headers?: Headers) => (req: Request, res: Response, next?: () => void) => {
-    if (req.method === "OPTIONS") {
-      res.set({
-        ...headers,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods":
-          "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":
-          "authorization, x-client-info, apikey, content-type, authorization-key, client_id, X-Authorization-Key",
-      });
+    res.set({
+      ...headers,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "authorization, x-client-info, apikey, content-type, authorization-key, client_id, X-Authorization-Key",
+    });
 
+    if (req.method === "OPTIONS") {
       res.send("ok");
     }
 
