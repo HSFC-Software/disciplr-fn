1c6dfd37165caa281e84adbc6ee19a5758a1ea9a
Sam <svtagalog@joyride.com.ph>
2023-07-13 14:32:14 +0800

    feat: added duplicate alert webhook for disciples table

--- supabase/functions/v2/_route/disciples.ts
+++ supabase/functions/v2/_route/disciples.ts
@@ -1,3 +1,4 @@
+import axiod from "https://deno.land/x/axiod/mod.ts";
 import express from "../../_shared/express.ts";
 import log from "../../_shared/log.ts";
 import { supabase } from "../../_shared/supabase-client.ts";
@@ -30,6 +31,29 @@ router
         );
       } catch (error) {
         log("Error getting profiles", req.path, error);
+      }
+
+      const tmp: { [key: string]: string[] } = {};
+      const duplicates: string[] = [];
+
+      results.forEach((item: any) => {
+        const name = `${item?.first_name} ${item?.last_name}`;
+        if (!tmp[name]) tmp[name] = [item.id];
+        else {
+          tmp[name].push(item.id);
+          if (!duplicates.includes(name)) duplicates.push(name);
+        }
+      });
+
+      const systemAlertUrl = Deno.env.get("SYSTEM_ALERT_URL") ?? "";
+      console.log({ systemAlertUrl });
+
+      for (const name of duplicates) {
+        const ids = tmp[name];
+        axiod.post(systemAlertUrl, {
+          name,
+          ids,
+        });
       }
 
       return res.send(results);
