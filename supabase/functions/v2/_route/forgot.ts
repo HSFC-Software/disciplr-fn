5f875a3bf67e119677e48b4dbb49661599720d74
Sam <svtagalog@joyride.com.ph>
2023-07-24 18:19:36 +0800

    feat: invalidate invitation after change password

--- supabase/functions/v2/_route/forgot.ts
+++ supabase/functions/v2/_route/forgot.ts
@@ -4,11 +4,67 @@ import { supabase } from "../../_shared/supabase-client.ts";
 import moment from "https://deno.land/x/momentjs@2.29.1-deno/mod.ts";
 import sendEmail from "../../_shared/email.ts";
 import shorten from "../../_shared/shorten.ts";
+import hash from "../../_shared/hash.ts";
 
 const router = express.Router();
 
 router
   .route("/v2/forgot") //
+  .get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const { data } = await supabase
+      .from("forgot_invitation")
+      .select("*")
+      .eq("id", id)
+      .maybeSingle();
+
+    if (!data) return res.status(404).send({});
+
+    if (data.expiration < moment().utc().toISOString())
+      return res.status(410).send({});
+
+    if (!data.link) return res.status(409).send({});
+    res.send(data);
+  })
+  .patch(async (req, res) => {
+    const { password, invitation_id } = req.body;
+
+    const { data } = await supabase
+      .from("forgot_invitation")
+      .select("auth_id")
+      .eq("id", invitation_id)
+      .single();
+
+    if (!data) {
+      log("Unable to find invitation to forgot password", req.baseUrl, {
+        invitation_id,
+      });
+      return res.status(404).send({});
+    }
+
+    const id = data?.auth_id;
+
+    const { error } = await supabase //
+      .from("auth")
+      .update({ password: hash(password) })
+      .eq("id", id);
+
+    if (error) {
+      log("Error updating password", req.baseUrl, { password, invitation_id });
+      return res.status(409).send({});
+    }
+
+    // invalidate invitation
+    await supabase
+      .from("forgot_invitation")
+      .update({
+        expiration: moment().utc().toISOString(),
+      })
+      .eq("id", invitation_id);
+
+    res.send({});
+  })
   .post(async (req, res) => {
     const { email } = req.body;
 
@@ -30,11 +86,15 @@ router
       .update({ expiration: now })
       .eq("auth_id", auth.id);
 
+    // create new auth invitation
+    const expiration = moment().add(7, "days").utc().toISOString();
+
     // create new invitation
     const { error, data } = await supabase
       .from("forgot_invitation")
       .insert({
         auth_id: auth.id,
+        expiration,
       })
       .select("id")
       .single();
@@ -93,7 +153,7 @@ Disciplr Team
     <p>Someone requested that we reset the password for your Disciplr account.</p>
     <div>If this was you, please follow this link:</div>
 
-    <button class="action">Reset Password</button>
+    <a href="${await shorten(email_redirect)}" class="action">Reset Password</a>
 
     <p>If this wasn't you, please report this e-mail to us at <a href="mailto:app.disiplr+support@gmail.com">app.disiplr+support@gmail.com</a></p>
 
